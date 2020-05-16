7c1d35d0fa90f4732e30ae2b16aceb880f5aeae0
JoseMaria <jmbernal@example.com>
2020-05-16 12:45:35 +0200

    KATA 1 - Javi Palanca

--- KATA1/ejercicio1.py
+++ KATA1/ejercicio1.py
@@ -15,14 +15,4 @@ print(f"Precio habitual: {precio}")
 print(f"\nDescuento: {precio_con_descuento}")
 print(f"\nCoste final: {numero_de_barras * precio_con_descuento}")
 
-# Ejercicio 2
-
-password = "contraseña"
-
-password_usuario = input("Introduzca la contraseña: ")
-
-if password == password_usuario:
-    print(f"SON IGUALES")
-else:
-    print(f"NO SON IGUALES")
 
