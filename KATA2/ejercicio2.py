595285558dfa5c0a185b3ddbac4d47ef8e3c5ae1
JoseMaria <jmbernal@example.com>
2020-05-16 12:54:53 +0200

    KATA 2 - Javi Palanca

--- KATA2/ejercicio2.py
+++ KATA2/ejercicio2.py
@@ -5,6 +5,6 @@ password = "contraseña"
 password_usuario = input("Introduzca la contraseña: ").lower()
 
 if password == password_usuario:
-    print("La contraseña es correcta")
+    print("La contraseña es correcta.")
 else:
-    print("La contraseña no es correcta")
+    print("La contraseña no es correcta.")
